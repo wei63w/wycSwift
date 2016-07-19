0bc25ca9b14c06c0a5068f639d7d85c5bf900891
customer <782385854@qq.com>
2016-07-19 13:52:26 +0800

    Create Pods
    Create Pods

--- wycSwift/wycSwift/ViewController.swift
+++ wycSwift/wycSwift/ViewController.swift
@@ -10,9 +10,52 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var firstbtn: UIButton!
+    @IBOutlet weak var secondBtn: UIButton!
+    @IBOutlet weak var thirdBtn: UIButton!
+    
+    
+    @IBAction func firstBtnTouch(sender: UIButton) {
+        
+        //var alertView = UIAlertView(title: "标题", message: getValWithType(0), delegate: self, cancelButtonTitle: "取消", otherButtonTitles: "确定", nil)
+        let alertController = UIAlertController(title: "标题", message: getValWithType(0), preferredStyle: UIAlertControllerStyle.Alert)
+        let cancelAction = UIAlertAction(title: "取消", style: UIAlertActionStyle.Cancel, handler: nil)
+        let okAction = UIAlertAction(title: "OK", style: UIAlertActionStyle.Default, handler: nil)
+        alertController.addAction(okAction)
+        alertController.addAction(cancelAction)
+        self.presentViewController(alertController, animated: true, completion: nil)
+        
+    }
+    
+    @IBAction func secondBtnTouch(sender: AnyObject) {
+        let alertController = UIAlertController(title: "标题", message: getValWithType(1), preferredStyle: UIAlertControllerStyle.Alert)
+        let cancelAction = UIAlertAction(title: "取消", style: UIAlertActionStyle.Cancel, handler: nil)
+        let okAction = UIAlertAction(title: "OK", style: UIAlertActionStyle.Default, handler: nil)
+        alertController.addAction(okAction)
+        alertController.addAction(cancelAction)
+        self.presentViewController(alertController, animated: true, completion: nil)
+    }
+    
+    @IBAction func thirdBtnTouch(sender: AnyObject) {
+        let alertController = UIAlertController(title: "标题", message: getValWithType(2), preferredStyle: UIAlertControllerStyle.Alert)
+        let cancelAction = UIAlertAction(title: "取消", style: UIAlertActionStyle.Cancel, handler: nil)
+        let okAction = UIAlertAction(title: "OK", style: UIAlertActionStyle.Default, handler: nil)
+        alertController.addAction(okAction)
+        alertController.addAction(cancelAction)
+        self.presentViewController(alertController, animated: true, completion: nil)
+    }
+   
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        self.firstbtn.backgroundColor = UIColor.redColor();
+        self.secondBtn.backgroundColor = UIColor.blueColor();
+        self.thirdBtn.backgroundColor = UIColor.greenColor();
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,5 +64,28 @@ class ViewController: UIViewController {
     }
 
 
+    
+    
+    func getValWithType(Type:Int32) -> String {
+        
+        var tempStr = "this is default"
+        
+        switch Type {
+        case 0:
+            tempStr = "This Type is 0"
+        case 1:
+            tempStr = "This Type is 1"
+        case 2:
+            tempStr = "This Type is 2"
+        default:
+            tempStr = "Last result Default"
+        }
+        
+        return tempStr;
+    }
+    
+    
+    
+    
 }
 
